3087e55d66c1238064ac80874e2cf639d4ae7ead
alexbarker234 <alex.barker234@gmail.com>
2023-05-06 21:10:55 +0800

    removed FK referential integrity

--- app/models.py
+++ app/models.py
@@ -32,19 +32,19 @@ class TrackArtist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     lastCacheDate = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
-    artist_id = db.Column(db.String(120), db.ForeignKey('artist.id'))
-    track_id = db.Column(db.String(120), db.ForeignKey('track.id'))
+    artist_id = db.Column(db.String(120)) # UNENFORCED FK
+    track_id = db.Column(db.String(120)) # UNENFORCED FK
 
 class TrackLyrics(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     last_cache_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     lyric_count = db.Column(db.Integer)
-    track_id = db.Column(db.String(120)) # loose foreign key 
+    track_id = db.Column(db.String(120)) # UNENFORCED FK
 
 class Lyric(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     order = db.Column(db.Integer)
     lyric = db.Column(db.String())
-    track_lyric_id = db.Column(db.String(120), db.ForeignKey('track_lyrics.id')) 
+    track_lyric_id = db.Column(db.String(120)) # UNENFORCED FK
