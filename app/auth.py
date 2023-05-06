a3e7405d2a24f10cd8b0c4ca718f991c8039372d
alexbarker234 <alex.barker234@gmail.com>
2023-05-06 23:18:34 +0800

    access check on login

--- app/auth.py
+++ app/auth.py
@@ -2,6 +2,7 @@
 from flask import session, redirect
 from app import app
 from app.helpers.spotify_helper import SpotifyHelper
+from spotipy.exceptions import SpotifyException
 
 @app.route('/login')
 def login():    
@@ -17,5 +18,19 @@ def logout():
 
 @app.route('/authorise')
 def authorise():
+
+    print("test1")
+
     SpotifyHelper.authorise()
+    print("test2")
+
+    # TEST IF THE USER HAS ACCESS
+    try:
+        SpotifyHelper().me()      
+    except SpotifyException as e:
+        if 'User not registered in the Developer Dashboard' in e.msg: 
+            for key in list(session.keys()):
+                session.pop(key)
+            return f"Please ask Alex for access - send email associated with spotify account\nBack to <a href='/'>index</a>"
+        
     return redirect("/")
