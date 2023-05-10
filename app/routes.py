fa14786b4ac335da1d4d632cf6f54dbeef4f981c
LLlads <23164864@student.uwa.edu.au>
2023-05-10 13:33:23 +0800

    Added route that receives score from game, and creates a new row (game) in the Stats table

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import redirect, render_template
-
-from app import app
-from app.helpers.spotify_helper import SpotifyWebUserData
+from datetime import datetime
+from app.models import Stats
+from flask import redirect, render_template, request
+from app import app, db
+from app.helpers.spotify_helper import SpotifyWebUserData, SpotifyHelper
 
 '''
 TODO:
@@ -33,3 +34,24 @@ def playlist_page(playlist_id):
 @app.route('/lyricgame/artist/<artist_id>')
 def artist_page(artist_id):
     return "not implemented"
+
+@app.route('/stats', methods=['POST'])
+def save_stats():
+    spotify_helper = SpotifyHelper()     
+    user_info = spotify_helper.me()
+    user_id = user_info['id']
+    score = request.form['score']
+
+    # Get the max game_id from the Stats table
+    max_game_id = Stats.query.order_by(Stats.game_id.desc()).first()
+    if max_game_id is None:
+        game_id = 1
+    else:
+        game_id = max_game_id.game_id + 1
+
+    # Create a new row in the Stats table
+    new_stats = Stats(game_id=game_id, user_id=user_id, streaknum=score, date_of_game=datetime.utcnow())
+    db.session.add(new_stats)
+    db.session.commit()
+
+    return 'Stats saved successfully.'
